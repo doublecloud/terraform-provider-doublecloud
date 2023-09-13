fad5d8a754a0ea3f2fb36e9c6dd11a8826c3d800
Ivan Leskin <in-leskin@double.cloud>
2023-09-13 17:33:17 +0200

    ORION-3611. Add an example terraform project for pg2ch transfer

---
+++ examples/resources/doublecloud_transfer_endpoint/resource.tf
@@ -0,0 +1,37 @@
+resource "doublecloud_transfer_endpoint" "sample-pg2ch-source" {
+  name = "sample-pg2ch-source"
+  project_id = var.project_id
+  settings {
+    postgres_source {
+      connection {
+        on_premise {
+          hosts = [
+            "mypostgresql.host"
+          ]
+          port = 5432
+        }
+      }
+      database = "postgres"
+      user = "postgres"
+      password = var.postgresql_postgres_password
+    }
+  }
+}
+
+resource "doublecloud_transfer_endpoint" "sample-pg2ch-target" {
+  name = "sample-pg2ch-target"
+  project_id = var.project_id
+  settings {
+    clickhouse_target {
+      clickhouse_cleanup_policy = "DROP"
+      connection {
+        address {
+            cluster_id = "chcexampleexampleexa"
+        }
+        database = "default"
+        password = var.clickhouse_admin_password
+        user = "admin"
+      }
+    }
+  }
+}
