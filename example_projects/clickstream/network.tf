ab1c1422f6b07dfbf7e44296d083e089c367b4a1
tserakhau <laskoviymishka@gmail.com>
2024-01-26 17:03:27 +0100

    Migrate example projects to https://github.com/doublecloud/terraform-doublecloud-examples

--- example_projects/clickstream/network.tf
+++
@@ -1,8 +0,0 @@
-// VPC for Kafka and Clickhouse networks
-resource "doublecloud_network" "main-network" {
-  project_id = var.project_id
-  name = "clickstream-network"
-  region_id = var.region_id
-  cloud_type = "aws"
-  ipv4_cidr_block = "10.0.0.0/16"
-}
