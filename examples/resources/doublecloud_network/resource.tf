f8565b0e93b13099ea2d9e7ba0eb2ef81479441d
Andrei Tserakhau <tserakhau@double.cloud>
2024-05-22 14:36:43 +0200

    Regenerate docs (#87)
    Doc generator for TF now do terraform format for examples so we must to
    regenerate whole docs.

--- examples/resources/doublecloud_network/resource.tf
+++ examples/resources/doublecloud_network/resource.tf
@@ -1,7 +1,7 @@
 resource "doublecloud_network" "example-network" {
-  project_id = var.project_id
-  name = "example-clickhouse"
-  region_id = "eu-central-1"
-  cloud_type = "aws"
+  project_id      = var.project_id
+  name            = "example-clickhouse"
+  region_id       = "eu-central-1"
+  cloud_type      = "aws"
   ipv4_cidr_block = "10.0.0.0/16"
 }
--- examples/resources/doublecloud_transfer/resource.tf
+++ examples/resources/doublecloud_transfer/resource.tf
@@ -1,17 +1,17 @@
 resource "doublecloud_transfer" "sample-pg2ch" {
-  name = "sample-pg2ch"
+  name       = "sample-pg2ch"
   project_id = var.project_id
-  source = doublecloud_transfer_endpoint.sample-pg2ch-source.id
-  target = doublecloud_transfer_endpoint.sample-pg2ch-target.id
-  type = "SNAPSHOT_ONLY"
-  activated = false
+  source     = doublecloud_transfer_endpoint.sample-pg2ch-source.id
+  target     = doublecloud_transfer_endpoint.sample-pg2ch-target.id
+  type       = "SNAPSHOT_ONLY"
+  activated  = false
   transformation = {
     transformers = [
       {
         dbt = {
           git_repository_link = "https://github.com/doublecloud/tests-clickhouse-dbt.git"
-          profile_name = "my_clickhouse_profile"
-          operation = "run"
+          profile_name        = "my_clickhouse_profile"
+          operation           = "run"
         }
       },
     ]
